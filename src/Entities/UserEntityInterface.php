14c077d5838ef02536ccc3b1ef1ea5493d883340
Seth Battis <SBattis@ml-tc-21-sbattis.local>
2022-09-05 18:59:38 +0000

    Make compatible with league/oauth2-server UserEntityInterface

--- src/Entities/UserEntityInterface.php
+++ src/Entities/UserEntityInterface.php
@@ -4,6 +4,6 @@ namespace Battis\UserSession\Entities;
 
 interface UserEntityInterface
 {
-  public function getIdentifier(): string;
+  public function getIdentifier();
   public function passwordVerify(string $password): bool;
 }
